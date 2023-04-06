50e3260e4200c84595990c4039830a4a429092ee
dpgil <daniel.patrick.gil@gmail.com>
2023-04-06 15:51:37 -0400

    Use disk in benchmarks

--- benches/insert.rs
+++ benches/insert.rs
@@ -7,7 +7,7 @@ use rstorage::{
 };
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("insert 100k series", |b| {
+    c.bench_function("insert", |b| {
         b.iter(|| {
             let data_path = String::from("./test_bench_data");
             let mut storage = Storage::new(Config {
@@ -39,10 +39,9 @@ fn criterion_benchmark(c: &mut Criterion) {
     });
 
     c.bench_function("select", |b| {
-        // Insert 100k data points
         let data_path = String::from("./test_bench_data");
         let mut storage = Storage::new(Config {
-            partition_duration: 10_000,
+            partition_duration: 1_000,
             insert_window: 100,
             data_path: data_path.clone(),
             encode_strategy: EncodeStrategy::CSV,
@@ -65,10 +64,10 @@ fn criterion_benchmark(c: &mut Criterion) {
             )
         }
 
-        let num_selects = 1000;
+        let num_selects = 10;
         b.iter(|| {
             for _ in 0..num_selects {
-                black_box(storage.select(metric, 0, 100_000));
+                black_box(storage.select(metric, 0, 100_000).unwrap());
             }
         });
 
