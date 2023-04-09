f6300643ae6104c2cb13a391509944b184ed4f4e
dpgil <daniel.patrick.gil@gmail.com>
2023-04-09 17:34:41 -0400

    buf

--- src/encode/gorilla.rs
+++ src/encode/gorilla.rs
@@ -5,43 +5,41 @@ use tsz::{stream::BufferedWriter, DataPoint, Encode, StdEncoder};
 use super::encode::{Encoder, Decoder};
 
 pub struct GorillaEncoder<W: Write + Seek> {
-    pub sink: BufWriter<W>,
-    // tsz's StdEncoder has its own buffering and doesn't write points
-    // until the encoder is closed. We could change the Encoder interface
-    // to accept all points at once, but for now we just buffer the incoming
-    // data points here and encode and write them at flush time.
-    pub buf: Vec<crate::DataPoint>,
+    pub writer: BufWriter<W>,
 }
 
 impl<W: Write + Seek> GorillaEncoder<W> {
     pub fn new(writable: W) -> Self {
         Self {
-            sink: BufWriter::new(writable),
-            buf: Vec::default(),
+            writer: BufWriter::new(writable),
         }
     }
 }
 
 impl<W: Write + Seek> Encoder for GorillaEncoder<W> {
     fn encode_points(&mut self, data_points: &[crate::DataPoint]) -> std::io::Result<()> {
+        if data_points.is_empty() {
+            return Ok(());
+        }
+
         let tsz_writer = BufferedWriter::new();
-        let mut tsz_encoder = StdEncoder::new(self.buf[0].timestamp as u64, tsz_writer);
-        for data_point in &self.buf {
+        let mut tsz_encoder = StdEncoder::new(data_points[0].timestamp as u64, tsz_writer);
+        for data_point in data_points {
             tsz_encoder.encode(DataPoint::new(
                 data_point.timestamp as u64,
                 data_point.value,
             ));
         }
         let bytes = tsz_encoder.close();
-        self.sink.write_all(&bytes)
+        self.writer.write_all(&bytes)
     }
 
     fn get_current_offset(&mut self) -> std::io::Result<u64> {
-        self.sink.seek(std::io::SeekFrom::Current(0))
+        self.writer.seek(std::io::SeekFrom::Current(0))
     }
 
     fn flush(&mut self) -> std::io::Result<()> {
-        self.sink.flush()
+        self.writer.flush()
     }
 }
 
